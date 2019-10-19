4184ab4ea8c56e02d81669ad13e4db4fb3746d87
Nathaniel Tjandra <ntjandra@ucsc.edu>
2019-10-19 14:06:41 -0700

    Added backend

---
+++ Flask/models.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
+from flask_login import UserMixin
+
+# Made by Team 16 at GS1 Tracathon 10/19/19
+
+@login_manager.user_loader
+# Safely cast the user_id to a int
+def load_manufacturer(user_id):
+    return Manufacturer.query.get(int(user_id))
+
+def load_supplier(user_id):
+    return Supplier.query.get(int(user_id))
+
+# Database Schema
+# We have two users, we store them seperately!
+class Manufacturer(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    # Create a One to many, One author, many posts.
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return Manufacturer.query.get(user_id)
+        
+    def __repr__(self):
+        return f"Manufacturer('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Supplier(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), unique=True, nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    
+    # Create a One to many, One author, many posts.
+    posts = db.relationship('Post', backref='author', lazy=True)
+    
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return Supplier.query.get(user_id)
+        
+    def __repr__(self):
+        return f"Supplier('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_shipped = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
